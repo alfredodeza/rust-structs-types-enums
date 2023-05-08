21a609e3ebbadf343b83eaa3bb967e9b78ae9c5b
Alfredo Deza <alfredo@deza.pe>
2023-05-08 19:47:24 -0400

    add examples 6 and 7

--- examples/6-strings/strings/src/main.rs
+++ examples/6-strings/strings/src/main.rs
@@ -1,3 +1,18 @@
+fn print_str(s: &str) {
+    let new_string = format!("{}! other stuff here", s);
+    println!("{}", new_string);
+}
+
+fn print_string(mut s: String) {
+    println!("{}", s);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = "hello, world!";
+    print_str(s);
+
+    // String is growable and mutable whereas str is not.
+    // String is owned by the code that creates it
+    let mut salutation = String::from("hello");
+    print_string(salutation);
 }
--- examples/7-mut-string/manipulating/src/main.rs
+++ examples/7-mut-string/manipulating/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let sentence = "the quick brown fox jumps over the lazy dog".to_string();
+    // Use slicing to get the first three characters of the sentence
+    //println!("{}", &sentence[0..=4]);
+
+    // concatenate using format!
+    let description = format!("Title: Quick story\n{}", sentence);
+    //println!("{}", description);
+
+    // iterate over the characters in the sentence
+    // for c in sentence.chars() {
+    //     match c {
+    //         'a' | 'e' | 'i' | 'o' | 'u' => println!("Got a vowel!"),
+    //         _ => continue,
+    //     }
+    // }
+
+    // Split and collect into a vector
+    //let words: Vec<&str> = sentence.split_whitespace().collect();
+    let words = sentence.split(' ').collect::<Vec<&str>>();
+    //println!("{:?}", words);
+
+    let reversed = sentence.chars().rev().collect::<String>();
+    println!("{}", reversed);
 }
