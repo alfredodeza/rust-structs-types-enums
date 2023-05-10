178f2363afcd523ca04c348489dc7e4f80142e70
Alfredo Deza <alfredo@deza.pe>
2023-05-10 07:39:36 -0400

    add last lesson

--- examples/16-exhaustive/exhaustive/src/main.rs
+++ examples/16-exhaustive/exhaustive/src/main.rs
@@ -1,3 +1,17 @@
+enum WineGrapes {
+    CabernetFranc,
+    Tannat,
+    Merlot,
+}
+
+fn taste_wine(grapes: WineGrapes) {
+    match grapes {
+        WineGrapes::CabernetFranc => println!("This is a Cabertnet Franc wine."),
+        // WineGrapes::Tannat => println!("This is a Tannat wine."),
+        // WineGrapes::Merlot => println!("This is a Merlot wine."),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    taste_wine(WineGrapes::CabernetFranc);
 }
